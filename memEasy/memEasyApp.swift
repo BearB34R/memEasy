88e261a19e423ad00992a391bd7863c0c449a874
BearB34R <andynguyendo@gmail.com>
2024-10-23 15:57:12 -0700

    Initial Commit

---
+++ memEasy/memEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  memEasyApp.swift
+//  memEasy
+//
+//  Created by Andy Do on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct memEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
